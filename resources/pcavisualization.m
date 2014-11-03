15807c6d094ae418913fd8822c75b192bd9501c5
ninad259 <ninadvij@buffalo.edu>
2014-11-03 01:06:24 -0500

    group scatter using instead of scatter

--- resources/pcavisualization.m
+++ resources/pcavisualization.m
@@ -7,4 +7,4 @@ label = data(1:rows, 2:2);
 coeff = princomp(features);
 multiplier = features-mean2(features);
 mxc = multiplier*coeff(:,1:2);
-scatter(mxc(:,1),mxc(:,2),3,label);
+gscatter(mxc(:,1),mxc(:,2),label);
